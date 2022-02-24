3ea22f6cbe634b8c31ef2e4433357ad8afe6d642
Rifki Amirul Hakim <rifkiamirulhakim@gmail.com>
2022-02-24 19:37:24 +0700

    map

--- 5_Basic Dart Program/dartfundamental2/bin/map.dart
+++ 5_Basic Dart Program/dartfundamental2/bin/map.dart
@@ -1,9 +1,21 @@
-void main(){
-  var student = {};
-  student['name'] = 'Alex under';
-  student['age'] = 16;
+// void main(){
+//   var student = {};
+//   student['name'] = 'Alex under';
+//   student['age'] = 16;
+//   print(student);
+
+//   print(student['name']);
+//   print(student['age']);
+// }
+
+void main() {
+  var student = {
+    'name' : 'alex under',
+    'age' : 16,
+  };
   print(student);
 
-  print(student['name']);
-  print(student['age']);
+  for(var key in student.keys) {
+    print(student[key]);
+  }
 }
