4341097cc6d6cea082b7d38058a45db2d88b725f
Rifki Amirul Hakim <rifkiamirulhakim@gmail.com>
2022-02-24 19:02:50 +0700

    menambahkan collection

---
+++ 5_Basic Dart Program/dartfundamental2/bin/collection.dart
@@ -0,0 +1,16 @@
+void main() async {
+  var scores = [];
+  print(scores);
+  scores.add(60);
+  scores.add(80);
+  scores.add(90);
+  scores.add(70);
+  scores.add(85);
+  print(scores);
+
+  print(scores[0]);
+  print(scores[1]);
+  print(scores[2]);
+  print(scores[3]);
+  print(scores[4]);
+}
