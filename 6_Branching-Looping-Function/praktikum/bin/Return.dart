cbc8ae2afc2542c75726b2a9f67391aae87f7729
Rifki Amirul Hakim <rifkiamirulhakim@gmail.com>
2022-03-01 09:01:43 +0700

    menambahkan fungsi return

---
+++ 6_Branching-Looping-Function/praktikum/bin/Return.dart
@@ -0,0 +1,18 @@
+void main (){
+  int angka = perangkaan(60, 70);
+  print(angka);
+
+}
+int perangkaan(int a, int b){
+  int angka = a+b;
+  return angka;
+}
+
+// void main (){
+//   var i=0;
+//   do{
+//     print(i);
+// //     i++;
+// //   } while(i<10);
+  
+// }
