10b70f884e8f409113723c02e43c78d641885f8d
Rifki Amirul Hakim <rifkiamirulhakim@gmail.com>
2022-03-10 09:52:36 +0700

    update

--- 10_Dart Object Oriented Programing 2/task/bin/task.dart
+++ 10_Dart Object Oriented Programing 2/task/bin/task.dart
@@ -1,3 +1,21 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+abstract class Matematika {
+  int hasil() {
+    return 0;
+  }
+}
+
+class Kpk implements Matematika {
+  @override
+  int hasil() {
+    int result = 0;
+    int x = 5;
+    int y = 9;
+    for (int i = 1; i <= y; i++) {
+      result = (result + x);
+      if (result % y == 0) {
+        return result;
+      }
+    }
+    return result;
+  }
 }
---
+++ 10_Dart Object Oriented Programing 2/task/bin/task2.dart
@@ -0,0 +1,24 @@
+abstract class BangunRuang {
+  int panjang = 10;
+  int lebar = 4;
+  int tinggi = 3;
+
+  int volume() {
+    return 0;
+  }
+}
+
+class Kubus extends BangunRuang {
+  int sisi = 9;
+  @override
+  int volume() {
+    return sisi * 3;
+  }
+}
+
+class Balok extends BangunRuang {
+  @override
+  int volume() {
+    return panjang * lebar * tinggi;
+  }
+}
