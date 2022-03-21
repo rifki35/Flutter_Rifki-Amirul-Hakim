fab90d6612cc9390c67c0e8361cd379c7ae3770a
Rifki Amirul Hakim <rifkiamirulhakim@gmail.com>
2022-03-21 10:24:52 +0700

    memindahkan file

---
+++ 13_Fluter CLI and flutter management/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:barcode_widget/barcode_widget.dart';;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+class HomePage extends StatelessWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: 
+      BarcodeWidget(
+      barcode: Barcode.ean13(),
+      data: 'Hello',
+      errorBuilder: (context, error) => Center(child: Text(error)),
+);,) ,
+    );
+  }
+}
