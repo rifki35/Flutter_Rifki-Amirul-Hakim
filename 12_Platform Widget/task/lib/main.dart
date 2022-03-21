1f925a53da2fdc7339ab1b3b844f103ffc28b8fa
Rifki Amirul Hakim <rifkiamirulhakim@gmail.com>
2022-03-21 19:27:47 +0700

    corection

--- 12_Platform Widget/task/lib/main.dart
+++ 12_Platform Widget/task/lib/main.dart
@@ -1,151 +1,162 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Home(),
-    );
-  }
-}
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0.0,
-        backgroundColor: Color(0xff55879F),
-        title: Text("Telegram",style: TextStyle(
-          fontSize: 25,
-          fontWeight: FontWeight.bold,
-          color: Colors.white
-        ),),
-        actions: [
-          Padding(
-              padding: EdgeInsets.only(right: 10),
-              child: Icon(Icons.search,size: 30,))
-        ],
-      ),
-
-      drawer: Drawer(
-        child: Column(
-          children: [
-            Container(
-              height: 250,
-              color: Colors.blue,
-              child: Container(
-                width: double.infinity,
-                child: Container(
-                  padding: EdgeInsets.symmetric(vertical: 50,horizontal: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      CircleAvatar(
-                        radius: 30,
-                        backgroundImage: ExactAssetImage('assets/one.jfif'),
+        appBar: AppBar(
+          title: Text('Telegram'),
+          centerTitle: false,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: List.generate(
+              Name.length,
+              (index) => Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16,
+                  vertical: 8,
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 55,
+                      width: 55,
+                      child: CircleAvatar(
+                        backgroundImage: NetworkImage(photos[0]),
                       ),
-                      SizedBox(height: 10,),
-                      Text("Alexandra Dardaria",style: TextStyle(color: Colors.white,fontSize: 18,fontWeight: FontWeight.w600),),
-                      SizedBox(height: 5,),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    ),
+                    SizedBox(
+                      width: 8,
+                    ),
+                    Expanded(
+                      child: Column(
                         children: [
-                          Text("+912569874563",style: TextStyle(color: Colors.white70,fontWeight: FontWeight.w600),),
-                          Icon(Icons.keyboard_arrow_down_outlined,size: 25,color: Colors.white,)
+                          Container(
+                            height: 55,
+                            child: Row(
+                              children: [
+                                Expanded(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        Name[0],
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 8,
+                                      ),
+                                      Text(
+                                        subtitles[0],
+                                        style: TextStyle(
+                                          fontSize: 12,
+                                          color: Colors.grey,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      clock[0],
+                                      style: TextStyle(
+                                        fontSize: 12,
+                                        color: Colors.grey,
+                                      ),
+                                    ),
+                                    Container(
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(100),
+                                          color: Colors.green),
+                                      padding: EdgeInsets.all(4),
+                                      width: 25,
+                                      height: 25,
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        '0',
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                        ),
+                                        ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                          Divider(),
                         ],
-                      )
-                    ],
-                  ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
-            tile(msg: "New Group",im: Icons.people),
-            tile(msg: "New Seceret Chat",im: Icons.lock),
-            tile(msg: "New Broadcast List",im: Icons.speaker_phone_outlined),
-            Divider(height: 10,),
-            tile(msg: "Contacts",im: Icons.contact_page_sharp),
-            tile(msg: "Theming",im: Icons.format_paint),
-            tile(msg: "Settings",im: Icons.settings),
-            tile(msg: "Telwgram FAQ",im: Icons.question_answer),
-          ],
-        ),
-      ),
-      body: Column(
-        children: [
-          list(url: 'assets/one.jfif',name: "Monika Goldsmith",tym: "1.57pm",desc: "Hallo",msg: "2",isRead: true),
-          Divider(height: 0.2,),
-          list(url: 'assets/two.jfif',name: "Kate",tym: "2.21pm",desc: "hallo apa kabar",msg: "",isRead: false),
-          Divider(height: 0.2,),
-          list(url: 'assets/three.jfif',name: "Agus",tym: "1.18pm",desc: "love u",msg: "2",isRead: true),
-          Divider(height: 0.2,),
-          list(url: 'assets/six.jfif',name: "Dani",tym: "2.31pm",desc: "whr r u baby?",msg: "2",isRead: true),
-          Divider(height: 0.2,),
-          list(url: 'assets/7.jfif',name: "Budi",tym: "3.25pm",desc: "missing u",msg: "",isRead: false),
-          Divider(height: 0.2,),
-          list(url: 'assets/8.jfif',name: "Badu",tym: "2.41pm",desc: "hai my love <3",msg: "2",isRead: true),
-          Divider(height: 0.2,),
-        ],
-      ),
-    );
-  }
-
-  ListTile tile({String msg,IconData im}) {
-    return ListTile(
-            leading: Icon(im,color: Colors.black54,size: 30,),
-            title: Text(msg,style: TextStyle(color: Colors.black54,fontSize: 18,fontWeight: FontWeight.w800),),
-          );
-  }
-
-  ListTile list({String url,String name,String tym,String desc,String msg,bool isRead}) {
-    return ListTile(
-      contentPadding: EdgeInsets.only(top: 5,left: 10),
-      leading: CircleAvatar(
-        radius: 30,
-        backgroundImage: ExactAssetImage(url),
-      ),
-      title: Padding(
-        padding: EdgeInsets.only(right: 10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(name,style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 19
-            ),),
-            Text(tym,style: TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 14
-            ),),
-          ],
-        ),
-      ),
-      subtitle: Padding(
-        padding: EdgeInsets.only(right: 10),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(desc,style: TextStyle(fontWeight: FontWeight.w500),),
-            Container(
-                child: Text(msg,style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 16),),
-                decoration: BoxDecoration(
-                  border: isRead ? Border.all(width: 1) : Border.all(width: 0),
-                  borderRadius: BorderRadius.circular(50),
-                  color: isRead ? Colors.green : Colors.white,
-                ),
-            )
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
+
+final Name = [
+  "Monica",
+  "Pricilia",
+  "Putri",
+  'Eka',
+  'Raon',
+  'lee',
+  'Kartika',
+  'Dewy',
+  'Jihan',
+  'Zahra'
+];
+
+final subtitles = [
+  "Photo",
+  "You have my support<3",
+  "Update?",
+  "Hi",
+  'Hello, what are you doing?',
+  'Beautyfull',
+  'Photo',
+  'waana go tonight?',
+  'What your name?',
+  'Where are you from?'
+];
+final clock = [
+  '10:10',
+  '09:10',
+  '07:10',
+  '05:10',
+  '05:16',
+  '05:16',
+  '05:16',
+  '05:16',
+  '03:16',
+  '01:16',
+];
+final photos = [
+  'https://pbs.twimg.com/media/EwbP3lwXEAABZOF.jpg',
+  'http://asset-a.grid.id/crop/0x0:0x0/x/photo/2018/10/22/3719332251.jpg',
+  'https://data.whicdn.com/images/254006393/original.jpg',
+  'https://i.pinimg.com/236x/39/fd/a5/39fda5781e5c77054251bfb5a25d451b.jpg',
+  'https://i.pinimg.com/236x/dd/ff/7a/ddff7af3424636a8fd8703ca7ce67166.jpg',
+  'https://i.pinimg.com/236x/c8/9a/a2/c89aa23f07b1464d3849db92ae73946f.jpg',
+  'https://i.pinimg.com/236x/48/87/59/4887599b2d4bd8b1a14530ea231b299e.jpg',
+  'https://i.pinimg.com/236x/ee/f3/ba/eef3bac54d574bf3c319d4dad06d6432.jpg',
+  'https://i.pinimg.com/236x/b4/e0/df/b4e0df6eb27dab7bc3c77260ae96a0d2.jpg',
+  'https://i.pinimg.com/236x/ca/7e/4f/ca7e4fc56b787bf9a6de53569bebc951.jpg'
+];
