9693d2275c5516c3d0859660ca6ad73ab38dbe42
Rifki Amirul Hakim <rifkiamirulhakim@gmail.com>
2022-04-12 12:29:50 +0700

    update

--- flutter_widget/android/app/src/main/kotlin/com/example/flutter_widget/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_widget
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- statemanagement/lib/person_model.dart
+++
@@ -1,6 +0,0 @@
-class PersonModel {
-  String name;
-  String nomorTelpon;
-
-  PersonModel({required this.name, required this.nomorTelpon});
-}
